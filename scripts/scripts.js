ab1cb6c25d04d95b40334c29e9521722edaa377b
JheysonGalvis <tecnojheyson@gmail.com>
2023-04-05 09:21:34 -0500

    se agrega Javascript al repositorio

---
+++ scripts/scripts.js
@@ -0,0 +1,3 @@
+nombre = prompt("Hola, cuál es tu nombre?")
+alert("Bienvenido. " + nombre + " Haz clic en una imágen para explorar más.")
+document.write("Bienvenido. " + nombre + " Haz clic en una imágen para explorar más.") 
